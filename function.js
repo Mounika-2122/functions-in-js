9625e0fcf071f26c3375e99037ebed371d8fe357
Mounika <bojjamounika8@gmail.com>
2021-12-16 00:09:34 +0530

    functions

---
+++ function.js
@@ -0,0 +1,28 @@
+  function nameofFunction () { 
+    //operation
+  } 
+console.log(typeof nameofFunction)
+
+  function greet() {  
+      console.log("hai everyone");
+  }
+  greet(); 
+
+  function greet(name){
+      console.log("hello" +name + "how are you");
+  }
+  var name=prompt("enter your name:");
+  greet(name);
+
+function add (a,b){
+    console.log(a+b);
+    return a + b ;
+}
+var result=add(3,4);
+console.log("the addition of two numbers is" +" "+ result);
+
+add (3,4);
+add(4,7);
+
+
+
